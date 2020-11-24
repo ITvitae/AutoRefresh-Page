4684fcfa972d3713d2ff2874c5359854c2f01ed4
Pieter <pieterbrandsen2@gmail.com>
2020-11-24 13:29:53 +0100

    Add project files.

---
+++ WebApplication1/Pages/Partials/Partial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Partials
+{
+    public class PartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
